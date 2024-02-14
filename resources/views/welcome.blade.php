6f620d86c1498010d66223deec31488476069a5f
jonathancode01 <icontatojonathan@gmail.com>
2024-02-14 13:03:08 -0300

    Projeto finalizado

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,28 +7,37 @@
 <div id="search-container" class="col-md-12">
         <h1>Busque um evento</h1>
 
-        <form action="">
+        <form action="/" method="GET">
                 <input type="text" id="search" name="search" class="form-control" placeholder="Procurar..." >
         </form>
 </div>
 
 <div id="events-container" class="col-md-12">
+        @if($search)
+        <h2>Buscando por: {{ $search }}</h2>
+        @else
         <h2>Proximos Eventos</h2>
         <p class="subtitle">Veja os eventos dos proximos dias</p>
-
+        @endif
         <div id="cards-conainter" class="row">
                 @foreach($events as $event)
                 <div class="card col-md-3">
-                        <img src="/img/event_placeholder.jpg" alt="{{ $event->title }}">
+                        <img src="/img/events/{{$event->image}}" alt="{{ $event->title }}">
                         <div class="card-body">
-                                <a class="card-date">22/01/2024</a>
+                                <p class="card-date"> {{ date('d/m/Y', strtotime($event->date))}} </p>
                                 <h5 class="card-title">{{ $event->title }}</h5>
-                                <p class="card-participants">X Participantes</p>
-                                <a href="#" class="btn btn-primary">Saber mais</a>
+                                <p class="card-participants">{{ count($event->users) }} Participantes</p>
+                                <a href="/events/{{$event->id}}" class="btn btn-primary">Saber mais</a>
 
                         </div>
                 </div>
                 @endforeach
+                @if(count($events) == 0 && $search) 
+                <p>Não foi possivel encontrar nenhum evento com {{ $search }}! <a href="/">Ver todos os Eventos</a></p>
+                @elseif(count($events) == 0)
+                <p>Não há eventos disponiveis</p>
+                @endif
+
         </div>
 </div>
      
